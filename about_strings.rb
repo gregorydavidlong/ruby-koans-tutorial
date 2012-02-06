57a1be4ba203a910159a87790062d6d1a9ca0c1b
Gregory Long <gregorydavidlong@gmail.com>
2012-02-07 09:01:31 +1100

    completed 129

--- about_strings.rb
+++ about_strings.rb
@@ -143,17 +143,17 @@ EOS
 
   def test_you_can_get_a_single_character_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal "a", string[1]
+    assert_equal 97, string[1]
 
     # Surprised?
   end
 
   in_ruby_version("1.8") do
     def test_in_ruby_1_8_single_characters_are_represented_by_integers
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal 97, ?a
+      assert_equal true, ?a == 97
 
-      assert_equal __, ?b == (?a + 1)
+      assert_equal true, ?b == (?a + 1)
     end
   end
 

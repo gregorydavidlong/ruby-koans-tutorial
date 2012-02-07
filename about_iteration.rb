5f52c368aa14db8767a102faf56b7921a18c7433
Gregory Long <gregorydavidlong@gmail.com>
2012-02-07 15:49:54 +1100

    Up to 167

--- about_iteration.rb
+++ about_iteration.rb
@@ -84,10 +84,10 @@ class AboutIteration < EdgeCase::Koan
 
   def test_inject_will_blow_your_mind
     result = [2, 3, 4].inject(0) { |sum, item| sum + item }
-    assert_equal __, result
+    assert_equal 9, result
 
     result2 = [2, 3, 4].inject(1) { |sum, item| sum * item }
-    assert_equal __, result2
+    assert_equal 24, result2
 
     # Extra Credit:
     # Describe in your own words what inject does.
